bbd8e167b87e2cfb0e37e6484140d07ca159f109
Rook Soto <rooksoto@gmail.com>
2021-02-15 14:01:56 -0500

    Adds Custom Cell Class

---
+++ Jornada/Common/Views/JornadaEntryTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  JornadaEntryTableViewCell.swift
+//  Jornada
+//
+//  Created by Rafael Soto on 2/15/21.
+//
+
+import UIKit
+
+class JornadaEntryTableViewCell: UITableViewCell {
+    
+    static let reuseIdentifier = "JornadaEntryTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // TODO: Set sizing (post-mvp - use normal cell for now)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // TODO: Set selection animation (post-mvp)
+    }
+
+}
