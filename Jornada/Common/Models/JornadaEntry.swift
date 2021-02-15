adf1c076e459b788fbe387efa5bd80672f5446ea
Rook Soto <rooksoto@gmail.com>
2021-02-15 13:51:41 -0500

    Adds JornadaEntry Class

---
+++ Jornada/Common/Models/JornadaEntry.swift
@@ -0,0 +1,17 @@
+//
+//  JornadaEntry.swift
+//  Jornada
+//
+//  Created by Rafael Soto on 2/15/21.
+//
+
+import Foundation
+
+class JornadaEntry: Codable {
+    
+    let id: String
+    let entry: String
+    let date: Date
+    
+    // TODO: - Add "Mood" and other fields
+}
